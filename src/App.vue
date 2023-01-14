b98a4bbf7375560a63904d39b3a79dc008d21734
y h <489693132@qq.com>
2023-01-14 07:21:02 -0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<script setup>
+import { ref, onMounted, watch  } from 'vue'
+import A2SCreatorVue from './components/A2SCreator.vue';
+const API_URL = "http://localhost:8000"
+
+const tableData = ref([])
+const drawer = ref(false)
+
+const refreshList = async () => {
+  const item = await (await fetch(API_URL + '/a2s/list')).json()
+  console.log(item)
+  tableData.value = item
+}
+
+watch(drawer, refreshList)
+
+onMounted(async () => {
+  console.log(`the list component is now mounted.`)
+  await refreshList()
+})
+
+const deleteById = async (id) => {
+  await fetch(API_URL + '/a2s/delete/?' + 'req_id=' + id.requestID)
+  console.log(id.requestID)
+  await refreshList()
+}
+
+</script>
+
+
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <el-button type="primary" style="margin-left: 16px" @click="drawer = true">
+          create
+        </el-button>
+      </el-header>
+      <el-main>
+        <el-scrollbar>
+          <el-table :data="tableData">
+            <el-table-column prop="expire" label="Expire" width="140" />
+            <!-- <el-table-column prop="callBackAddr" label="CallBack" /> -->
+            <el-table-column prop="proxyAddr" label="ProxyAddr" />
+            <el-table-column prop="requestID" label="Address" />
+            <el-table-column prop="requestID" label="Address">
+              <template #default="scope">
+                <el-button type="danger" @click="deleteById(scope.row)">delete</el-button>
+              </template>
+            </el-table-column>
+            
+          </el-table>
+        </el-scrollbar>
+        <el-drawer v-model="drawer" title="I am the title" :with-header="false" size="50%">
+          <span>######Create New#######</span>
+          <div>
+            <A2SCreatorVue></A2SCreatorVue>
+          </div>
+        </el-drawer>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<style scoped>
+.layout-container-demo .el-header {
+  position: relative;
+  background-color: var(--el-color-primary-light-7);
+  color: var(--el-text-color-primary);
+}
+.layout-container-demo .el-menu {
+  border-right: none;
+}
+.layout-container-demo .el-main {
+  padding: 0;
+}
+.layout-container-demo .toolbar {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  right: 20px;
+}
+</style>
---
+++ src/components/A2SCreator.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref } from 'vue'
+import { Check, Close } from '@element-plus/icons-vue'
+const echoProxy = ref(true)
+const randomCallBack = ref(true)
+const proxyAddr = ref("")
+const callBackAddr = ref("")
+const API_URL_PREFIX = 'http://127.0.0.1:8000'
+
+async function create(event) {
+    const API_URL = API_URL_PREFIX + `/a2s/create`
+    const req_body = {
+        echoProxy: echoProxy.value,
+        randomCallBack: randomCallBack.value,
+        proxyAddr: proxyAddr.value,
+        callBackAddr: callBackAddr.value,
+    }
+    const res = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(req_body)
+    })
+    console.log(await res.json())
+}
+</script>
+
+<template>
+    
+    <span>EchoProxy: </span>
+    <el-switch v-model="echoProxy" :active-icon="Check" :inactive-icon="Close" inline-prompt />
+    <br/>
+
+    <span>ProxyAddr: </span>
+    <el-input v-model="proxyAddr" :disabled="echoProxy" placeholder="Please input" />
+    <br/>
+    <br/>
+
+    <span>RandomCallBack: </span>
+    <el-switch v-model="randomCallBack" :active-icon="Check" :inactive-icon="Close" inline-prompt />
+    <br/>
+
+    <span>CallBackAddr: </span>
+    <el-input v-model="callBackAddr" :disabled="randomCallBack" placeholder="Please input" />
+    
+    <el-button type="primary" style="margin-top: 2em;" @click="create">
+        create
+    </el-button>
+    
+</template>
